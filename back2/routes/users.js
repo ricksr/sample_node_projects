15b66b303d23bf014485c5d76bc795b3d3cefb45
ricksr <sutirtharej@gmail.com>
2020-10-19 16:41:06 +0530

    logged-in user verified

--- back2/routes/users.js
+++ back2/routes/users.js
@@ -57,4 +57,25 @@ router.post('/login', (req, res) => {
     res.status(400).json('Error : ' + 'Login not Successfull')
 });
 
+router.post('/posts', verifyToken, (req, res) => {
+    if (req.length !== 2) res.status(400).json('Error : ' + 'Auth. Failed')
+    jwt.verify(req[1].token, 'secretkey', (err, authData) => {
+        err ? res.status(403).json('Error : ' + 'Auth. Failed') :
+            res.status(200).json([{ 'Success': 'loggedin user verified' }])
+    })
+});
+
+const verifyToken = async (req, res, next) => {
+    const brearerHeader = req.headers['authorization'];
+    if (typeof brearerHeader !== 'undefined') {
+        const bearer = brearerHeader.split(' ');
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        let response = await next();
+        // console.log('\n' + response + '\n');
+    } else {
+        res.status(403).json('Error : ' + 'Auth. Failed, access Forbidden')
+    }
+};
+
 module.exports = router;
